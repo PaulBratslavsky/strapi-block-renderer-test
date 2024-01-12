c49a7463aebde258132f76f7df6b65f1164867df
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2024-01-12 12:29:21 -0600

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,5 @@
-import {
-  BlocksRenderer,
-  type BlocksContent,
-} from "@strapi/blocks-react-renderer";
+import { type BlocksContent } from "@strapi/blocks-react-renderer";
+import BlockRendererClient from "./BlockRendererClient";
 
 async function getStrapiData() {
   const res = await fetch("http://localhost:1337/api/notes");
@@ -17,7 +15,7 @@ export default async function Home() {
   return (
     <main className="max-w-[980px] mx-auto my-36">
       <h1>{data[0].attributes.title}</h1>
-      <BlocksRenderer content={content} />
+      <BlockRendererClient content={content} />
     </main>
   );
 }
